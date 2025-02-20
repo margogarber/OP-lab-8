fed330bfd76afed1127dc599d29bc4bd97b2c749
Margarita Garber <margogarber@outlook.com>
2025-02-20 23:41:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        // Створення об'єкта класу-нащадка
+
         Smartphone phone1 = new Smartphone("Apple", "iPhone 16", "iOS", 512);
         Smartphone phone2 = new Smartphone("Samsung", "Galaxy S21", "Android", 256);
 
-        // Демонстрація використання this, super, перевантаження методів та конструкторів
-        System.out.println(phone1); // Виклик toString()
+
+        System.out.println(phone1); 
         phone1.makeCall("123456789");
         phone1.browseInternet("example.com");
         phone1.browseInternet("example.com", true);
 
-        // Порівняння об'єктів
+
         System.out.println("Phones are equal: " + phone1.equals(phone2));
 
-        // Демонстрація використання this, super, перевантаження методів та конструкторів
-        System.out.println(phone2); // Виклик toString()
+  
+        System.out.println(phone2); 
         phone2.makeCall("123456789");
         phone2.browseInternet("example.com");
         phone2.browseInternet("example.com", true);
 
-        // Порівняння об'єктів
+
         System.out.println("Phones are equal: " + phone2.equals(phone1));
     }
 }
