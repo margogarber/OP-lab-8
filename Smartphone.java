2af28032138ceb4303d3b0cfb8588ce277c51944
Margarita Garber <margogarber@outlook.com>
2025-02-20 23:42:24 +0200

    Update Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,32 +1,29 @@
-// Клас-нащадок "Смартфон"
-public class Smartphone extends Phone {
-    private final String os; // Незмінний атрибут
-    private final int storage; // Незмінний атрибут
 
-    // Конструктор з параметрами
+public class Smartphone extends Phone {
+    private final String os; 
+    private final int storage; 
+
     public Smartphone(String brand, String model, String os, int storage) {
-        super(brand, model); // Виклик конструктора базового класу
+        super(brand, model); 
         this.os = os;
         this.storage = storage;
     }
 
-    // Перевантаження конструктора
+
     public Smartphone(String brand, String model) {
-        this(brand, model, "Unknown OS", 64); // Виклик іншого конструктора
+        this(brand, model, "Unknown OS", 64); 
     }
 
-    // Перевантаження методу makeCall
     @Override
     public void makeCall(String number) {
         System.out.println("Calling " + number + " using smartphone...");
     }
 
-    // Метод бізнес-логіки
     public void browseInternet(String website) {
         System.out.println("Browsing website: " + website);
     }
 
-    // Перевантаження методу browseInternet (перевантаження)
+   
     public void browseInternet(String website, boolean incognito) {
         if (incognito) {
             System.out.println("Browsing website in incognito mode: " + website);
@@ -35,7 +32,7 @@ public class Smartphone extends Phone {
         }
     }
 
-    // Реалізація toString()
+
     @Override
     public String toString() {
         return "Smartphone{" +
@@ -46,7 +43,6 @@ public class Smartphone extends Phone {
                 '}';
     }
 
-    // Реалізація equals()
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
